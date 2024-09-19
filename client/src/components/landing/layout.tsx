1ade52b67b999dbff1f6b6212b9c70e13d9b8731
johnmiicheal <michealelijah301@gmail.com>
2024-09-19 10:06:54 +0100

    feat(landing): landing page init commit + assets

---
+++ client/src/components/landing/layout.tsx
@@ -0,0 +1,33 @@
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import FancyButton from "../ui/button";
+
+interface LandingLayoutProps {
+  children: React.ReactNode;
+}
+
+export const LandingLayout: React.FC<LandingLayoutProps> = ({ children }) => {
+  return (
+    <Flex direction="column" pos="relative" px={10} gap={20} h="100vh" align="center" bg="#FFF9E5" bgImg="/assets/oja-bg.png" bgSize="contain" bgPos="top">
+      <Flex
+        maxW="1650px"
+        w="full"
+        bg="none"
+        justify="space-between"
+        p={2}
+        align="center"
+        h="135px"
+        pos="sticky"
+      >
+        <Flex align="center">
+          <Image src="/icons/ojami-logo.svg" alt="ojami logo" w="80px" />
+          <Image src="/assets/oja-kora.svg" alt="oja kora" w="120px" transform="rotate(-15deg)" />
+        </Flex>
+        <FancyButton bg="/assets/buttons/small-flower.svg" w={"100px"} h={"200px"}>
+            my cart
+        </FancyButton>
+      </Flex>
+      {children}
+    </Flex>
+  );
+};
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
+import localFont from "next/font/local";
 
+const general = localFont({
+  src: "./fonts/GeneralSans-Variable.woff2",
+  weight: "100 900",
+  variable: "--font-general-sans",
+});
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Sell anything" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icons/oja.svg" />
+      </Head>
+      <main className={`${general.className}`}>
+        <Component {...pageProps} />
+      </main>
     </ChakraProvider>
   );
 }
-
